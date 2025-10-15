b24fdd7f606d571ded20b979489edf51f858f324
Kreker1488 <shprotakerill@gmail.com>
2025-10-15 16:35:05 +0200

    generate_map

--- main.cpp
+++ main.cpp
@@ -1,17 +1,38 @@
 #include <iostream>
+#include <cstdlib>
+
+void generate() {
+	char matrix[3][4];
+	int x;
+	for (int stroki = 0; stroki < 3; ++stroki) {
+		std::cout << std::endl;
+		for (int stolbi = 0; stolbi < 4; ++stolbi) {
+			x = (rand() % 3);
+			switch (x)
+			{
+			case 0:
+				matrix[stroki][stolbi] = '0';
+				break;
+			case 1:
+				matrix[stroki][stolbi] = '*';
+				break;
+			case 2:
+				matrix[stroki][stolbi] = '@';
+				break;
+			default:
+				break;
+			}
+
+
+			//matrix[stroki][stolbi] = x;
+			std::cout << matrix[stroki][stolbi] << ' ';
+		}
+	}
+	
+}
+
 
 int main()
 {
-	int matrix[3][4];
-	int numerate = 0;
-	for (int i = 0; i < 3; ++i){
-		std::cout << std::endl;
-		for (int j = 0; j < 4; ++j){
-			matrix[i][j] = numerate;
-			++numerate;
-			std::cout << matrix[i][j] << ' ';
-		}
-	}
-
-	
+	generate();
 }
