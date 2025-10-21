ecbf87034ccc37b7ec0bafdfd2ec5353e350ee31
DAZhuravskiy <drugoydimaa@gmail.com>
2025-10-21 22:37:38 +0200

    Add: map logic
    Добавлена генерация случайной карты 10x10, генерация персонажа в случайном месте, генерация препятствий

--- map.cpp
+++ map.cpp
@@ -1,39 +1,37 @@
 #include <iostream>
 #include <cstdlib>
-#include <time.h>
-#include <vector>
-int generate() {
-	// так как значение массива( типа matrix[4][5]) нормально вытащить из функции нельзя( ну или можно но костялями какимито) разумней будет использовать для генерации карты std::vector
-	srand(time(NULL)); //вот эта штука позволяет реально рандомить карту, а не выводить одни и те же комбинации
-	int len = 10;
-	std::vector<char> matrix(len * len, '0');// здесь мы задает вектора matrix который по сути будет нашей картой в виде цепочки нулей.
-	for (int i = 0; i < len * len - 1;) {
-		std::cout << std::endl;
-		int x;
 
-		for (int k = 0; k < len; k++) {
-			x = rand() % 3;
-			switch (x) {
-			case 0: matrix[i] = '0'; break;
-			case 1: matrix[i] = '*'; break;
-			case 2: matrix[i] = '0'; break;
+void generate() {
+  srand(time(NULL)); //вот эта штука позволяет реально рандомить карту, а не выводить одни и те же комбинации
+  char matrix[10][10]; //создаем матрицу, выполняющую роль карты
+  int x; //переменные, принимающие случайное значение для заполнения карты объектами
+  int y;
 
-			}
-			i++;
-			//т.к. вектор является цепочкой из элементов, для генерации случайных карт нужно зарандомить значение каждого элемента этой последовательности
-		}
-	}
-	for (int i = 0; i < len * len - 1 - 1;) {
+  for (int stroki = 0; stroki < 10; ++stroki) { //создаем матрицу, полностью состоящую из нулей
+    std::cout << std::endl;
+    for (int stolbi = 0; stolbi < 10; ++stolbi) {
+      matrix[stroki][stolbi] = '0';
+    }
+  }
 
-		std::cout << std::endl;
-		for (int k = 0; k < len; k++) {
-			std::cout << matrix[i] << ' ';
-			i++;
-		}
-	}
-	return 0;
+  for (int i = 0; i < 20; i++){ //добавдяем в нее нужное кол-во камней (второй аргумент цикла отвечает за это кол-во)
+    x = (rand() % 10);
+    y = (rand() % 10);
+    matrix[x][y] = '#';
+  }
+  for (int i = 0; i < 1; i++){ //добавляем нужное кол-во персонажей
+    x = (rand() % 10);
+    y = (rand() % 10);
+    matrix[x][y] = '@';
+  }
+
+  for (int i = 0; i<10; i++){ //выводим матрицу
+    std::cout << std::endl;
+    for (int j = 0; j<10; j++){
+      std::cout << matrix[i][j] << ' ';
+    }
+  }
 }
 int main(){
-
-	generate();
+  generate();
 }
