7b39453fe866a67add0a07bf8653540a909a56c1
Kreker1488 <shprotakerill@gmail.com>
2025-10-22 14:50:20 +0200

    Update WASD.cpp
    system("clear") заменен на cout << "\033[2J"
    добавлен setlocale

--- WASD.cpp
+++ WASD.cpp
@@ -3,69 +3,72 @@
 #include <conio.h>
 
 void generate() {
-  srand(time(NULL)); //вот эта штука позволяет реально рандомить карту, а не выводить одни и те же комбинации
-  char matrix[10][10]; //создаем матрицу, выполняющую роль карты
-  int x; //переменные, принимающие случайное значение для заполнения карты объектами
-  int y;
+    srand(time(NULL)); //вот эта штука позволяет реально рандомить карту, а не выводить одни и те же комбинации
+    char matrix[10][10]; //создаем матрицу, выполняющую роль карты
+    int x; //переменные, принимающие случайное значение для заполнения карты объектами
+    int y;
 
-  for (int stroki = 0; stroki < 10; ++stroki) { //создаем матрицу, полностью состоящую из нулей
-    std::cout << std::endl;
-    for (int stolbi = 0; stolbi < 10; ++stolbi) {
-      matrix[stroki][stolbi] = ' ';
-    }
-  }
-
-  for (int i = 0; i < 20; i++){ //добавдяем в нее нужное кол-во камней (второй аргумент цикла отвечает за это кол-во)
-    x = (rand() % 10);
-    y = (rand() % 10);
-    matrix[x][y] = '#';
-  }
-
-  int playerX;
-  int playerY;
-  do {
-    playerX = (rand() % 10);
-    playerY = (rand() % 10);
-  } while (matrix[playerX][playerY] == '#'); //проверяем чтобы клетка не была занята "камнем"
-  matrix[playerX][playerY] = '@';
-
-  auto printMap = [&](void){ //создаем функцию внутри фнкции (лямбда-функция)
-    //auto сам определяет переменную printMap, [&] позволяет видеть переменные из внешней функци 
-    for (int i = 0; i<10; i++){ //выводим матрицу
+    for (int stroki = 0; stroki < 10; ++stroki) { //создаем матрицу, полностью состоящую из нулей
         std::cout << std::endl;
-        for (int j = 0; j<10; j++){
-            std::cout << matrix[i][j] << ' ';
+        for (int stolbi = 0; stolbi < 10; ++stolbi) {
+            matrix[stroki][stolbi] = ' ';
+        }
     }
-  }
-  };
-  
-    while(true) {
-  system("clear");
-    printMap();
-    char move = getch(); //не ждем нажатия enter после ввода символа
-    move = std::tolower(move);
 
-    if (move == 'q'){
-        std::cout << "Выход из игры\n";
-        break;
+    for (int i = 0; i < 20; i++) { //добавдяем в нее нужное кол-во камней (второй аргумент цикла отвечает за это кол-во)
+        x = (rand() % 10);
+        y = (rand() % 10);
+        matrix[x][y] = '#';
     }
-    int newX = playerX;
-    int newY = playerY;
-    if (move == 'w') newX--;
-    else if (move == 'a') newY--;
-    else if (move == 's') newX++;
-    else if (move == 'd') newY++;
-    else continue;
 
-    if (newX >= 0 && newX < 10 && newY >= 0 && newY < 10 && matrix[newX][newY] != '#') { //ограничения по краям карты и камням
-        matrix[playerX][playerY] = ' ';
-        playerX = newX;
-        playerY = newY;
-        matrix[playerX][playerY] = '@';
-    }
-    }
-  }
+    int playerX;
+    int playerY;
+    do {
+        playerX = (rand() % 10);
+        playerY = (rand() % 10);
+    } while (matrix[playerX][playerY] == '#'); //проверяем чтобы клетка не была занята "камнем"
+    matrix[playerX][playerY] = '@';
 
-int main(){
-  generate();
+    auto printMap = [&](void) { //создаем функцию внутри фнкции (лямбда-функция)
+        //auto сам определяет переменную printMap, [&] позволяет видеть переменные из внешней функци 
+        for (int i = 0; i < 10; i++) { //выводим матрицу
+            std::cout << std::endl;
+            for (int j = 0; j < 10; j++) {
+                std::cout << matrix[i][j] << ' ';
+            }
+        }
+    };
+
+    while (true) {
+        //system("clear"); //эта штука странно рботает в зависимости от запуска, да и дипсик на нее ругается, так что пох
+        std::cout << "\033[2J"; //вот эта имбулька очищает экран от кала
+
+        printMap();
+        char move = _getch(); //не ждем нажатия enter после ввода символа
+        move = std::tolower(move);
+
+        if (move == 'q') {
+            std::cout << "Выход из игры\n";
+            break;
+        }
+        int newX = playerX;
+        int newY = playerY;
+        if (move == 'w') newX--;
+        else if (move == 'a') newY--;
+        else if (move == 's') newX++;
+        else if (move == 'd') newY++;
+        else continue;
+
+        if (newX >= 0 && newX < 10 && newY >= 0 && newY < 10 && matrix[newX][newY] != '#') { //ограничения по краям карты и камням
+            matrix[playerX][playerY] = ' ';
+            playerX = newX;
+            playerY = newY;
+            matrix[playerX][playerY] = '@';
+        }
+    }
+}
+
+int main() {
+    setlocale(LC_ALL, "Russian");
+    generate();
 }
