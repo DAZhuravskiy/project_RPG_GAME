b68f9a7e5c4cded970ee6d5ef89618a017c14e9c
DAZhuravskiy <drugoydimaa@gmail.com>
2025-10-28 23:46:07 +0200

    Перенос кода в main
    Так как изначально делали неправильно, сейчас делаем ветки от main и потом коммитим

--- main.cpp
+++ main.cpp
@@ -1,37 +1,113 @@
 #include <iostream>
 #include <cstdlib>
-#include <time.h>
+#include <conio.h>
+
 void generate() {
-	srand(time(NULL)); //вот эта штука позволяет реально рандомить карту, а не выводить одни и те же комбинации
-	char matrix[7][9];
-	int x;
-	for (int stroki = 0; stroki < 7; ++stroki) {
-		std::cout << std::endl;
-		for (int stolbi = 0; stolbi < 9; ++stolbi) {
-			x = (rand() % 3);
-			switch (x)
-			{
-			case 0:
-				matrix[stroki][stolbi] = '0';
-				break;
-			case 1:
-				matrix[stroki][stolbi] = '*';
-				break;
-			case 2:
-				matrix[stroki][stolbi] = '@';
-				break;
-			default:
-				break;
-			}
+    srand(time(NULL)); //вот эта штука позволяет реально рандомить карту, а не выводить одни и те же комбинации
+    char matrix[10][10]; //создаем матрицу, выполняющую роль карты
+    int x; //переменные, принимающие случайное значение для заполнения карты объектами
+    int y;
+    int hp = 3; //Здоровье персонажа
+    int coins = 0; // Баланс игрока
 
+    for (int i = 0; i < 10; ++i) { //создаем матрицу, полностью состоящую из нулей
+        std::cout << std::endl;
+        for (int j = 0; j < 10; ++j) {
+            matrix[i][j] = ' ';
+        }
+    }
 
-			//matrix[stroki][stolbi] = x;
-			std::cout << matrix[stroki][stolbi] << ' ';
-		}
-	}
+    for (int i = 0; i < 10; i++) { //добавдяем в нее нужное кол-во камней (второй аргумент цикла отвечает за это кол-во)
+        x = (rand() % 10);
+        y = (rand() % 10);
+        matrix[x][y] = '#';
+    }
+
+    for (int i = 0; i < 5; i++) { //добавдяем в нее нужное кол-во шипов
+        x = (rand() % 10);
+        y = (rand() % 10);
+        matrix[x][y] = '*';
+    }
+
+    for (int i = 0; i < 5; i++) { //добавдяем в нее нужное кол-во монеток
+        x = (rand() % 10);
+        y = (rand() % 10);
+        matrix[x][y] = '$';
+    }
+
+    int playerX;
+    int playerY;
+    do {
+        playerX = (rand() % 10);
+        playerY = (rand() % 10);
+    } while (matrix[playerX][playerY] == '#'); //проверяем чтобы клетка не была занята "камнем"
+    matrix[playerX][playerY] = '@';
+
+    auto printMap = [&](void) { //создаем функцию внутри фнкции (лямбда-функция)
+        //auto сам определяет переменную printMap, [&] позволяет видеть переменные из внешней функци 
+        std::cout << "Текущее здоровье = " << hp <<  "Текущее баланс = " << coins  std::endl; //вывод количества здоровья
+        for (int i = 0; i < 10; i++) { //выводим матрицу
+            std::cout << std::endl;
+            for (int j = 0; j < 10; j++) {
+                std::cout << matrix[i][j] << ' ';
+            }
+        }
+    };
+
+    while (true) {
+        //system("clear"); //эта штука странно рботает в зависимости от запуска, да и дипсик на нее ругается, так что пох
+        std::cout << "\033[1J"; //вот эта имбулька очищает экран от кала
+
+        printMap();
+        char move = _getch(); //не ждем нажатия enter после ввода символа
+        move = std::tolower(move);
+
+        if (move == 'q') {
+            std::cout << "Выход из игры\n";
+            break;
+        }
+
+        if (move == 'e') {
+            std::cout << "Ваш баланс монет: " << coins << std::endl;
+            continue;
+        }
+
+        int newX = playerX;
+        int newY = playerY;
+        if (move == 'w') newX--;
+        else if (move == 'a') newY--;
+        else if (move == 's') newX++;
+        else if (move == 'd') newY++;
+        else continue;
+
+        if (matrix[newX][newY] == '$') {
+            coins++;
+        }
+
+        if (newX >= 0 && newX < 10 && newY >= 0 && newY < 10 && matrix[newX][newY] != '#' && matrix[newX][newY] != '*') { //ограничения по краям карты и камням и шипам
+            matrix[playerX][playerY] = ' ';
+            playerX = newX;
+            playerY = newY;
+            matrix[playerX][playerY] = '@';
+        }
+        if (newX >= 0 && newX < 10 && newY >= 0 && newY < 10 && matrix[newX][newY] == '*'){ //если игрок сталкивается с шипами, то здоровье уменьшается
+            hp--;
+        }
+        if (hp == 0) { //если хп = 0, выход из игры
+            std::cout << "Выход из игры\n";
+            break;
+        }
+    }
 }
-int main()
 
-{
-	generate();
+int main() {
+    setlocale(LC_ALL, "Russian");
+    std::cout << "q - выход из игры" << std::endl;
+    std::cout << "wasd - передвижение" << std::endl;
+    std::cout << "@ - это вы" << std::endl;
+    std::cout << "# - это камни, на них наступить не получится)" << std::endl;
+    std::cout << "* - это шипы, они отнимают здоровье)" << std::endl;
+     std::cout << "e - показать баланс монет" << std::endl;
+    std::cout << "$ - монетки, прибавляют баланс" << std::endl;
+    generate();
 }
